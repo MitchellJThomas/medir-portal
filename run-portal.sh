7f00353bd45d1329c4980fab2b80c3b5e837e148
Mitch Thomas <mthomas@tripwire.com>
2020-03-23 15:49:43 -0700

    Additional details to run as non-root

--- run-portal.sh
+++ run-portal.sh
@@ -1,6 +1,7 @@
 mkdir datum  >/dev/null 2>&1
 mkdir certs >/dev/null 2>&1
 docker rm -f website
-docker pull mitchelljthomas/medirportal_go:latest
-# docker run -d --memory=256m -p 80:80 -p 443:443 --name website --volume $PWD/datum:/medir/datum --volume $PWD/certs:/medir/certs mitchelljthomas/medirportal_go:latest
-docker run -d --memory=256m -p 80:80 -p 443:443 --name website --volume $PWD/datum:/medir/datum --volume $PWD/certs:/medir/certs mitchelljthomas/medir-portal:0.2.57
+
+RELEASE=104-master-76ffc4fe6b22819c1cb5b83bc86888ef58a3f7b6
+
+docker run -d --memory=256m -p 80:80 -p 443:443 -u 1000 --name website --volume $PWD/datum:/medir/datum --volume $PWD/certs:/medir/certs mitchelljthomas/medir-portal:$RELEASE
