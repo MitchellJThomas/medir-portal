90f13812fa4756f2ea228addbda26a943d12c5b2
Mitch Thomas <mthomas@tripwire.com>
2020-03-28 13:45:09 -0700

    Add restart on failure flag

--- run-portal.sh
+++ run-portal.sh
@@ -4,4 +4,4 @@ docker rm -f website
 
 RELEASE=<docker image>
 
-docker run -d --memory=256m -p 80:8080 -p 443:8443 -u 1000 --name website --volume $PWD/datum:/medir/datum --volume $PWD/certs:/medir/certs mitchelljthomas/medir-portal:$RELEASE
+docker run -d --memory=256m -p 80:8080 -p 443:8443 -u 1000 --read-only --restart=on-failure:10 --name website --volume $PWD/datum:/medir/datum --volume $PWD/certs:/medir/certs mitchelljthomas/medir-portal:$RELEASE
