4b7e7061e40e0c3dfd12d75c7ebbcfc883f1c245
Mitch Thomas <mthomas@tripwire.com>
2021-03-17 18:13:33 -0700

    It worked! Switch back to the usual

--- ui/medir-ui/src/App.js
+++ ui/medir-ui/src/App.js
@@ -224,7 +224,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-	<h1>IttySensor From Fleek and IPFS!</h1>
         <Nav />
         <DeviceTable />
       </div>
