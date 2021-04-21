6b37df809ac9b1a79f57be1cf9ccda30c73826bd
Mitch Thomas <mthomas@tripwire.com>
2021-04-20 21:16:29 -0700

    Remove the systemd config files, they are in wifi-outlet-timer

--- resources/lights-off.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-access_token=abc123
-
-echo "$(date -Iseconds) front off $(curl -s -XPOST -H "Authorization: Bearer $access_token" https://api.particle.io/v1/devices/1e003d000d47343233323032/relayOff -d arg="3")" >> /var/log/lights.log
-
-echo "$(date -Iseconds) wreath off $(curl -s -XPOST -H "Authorization: Bearer $access_token" https://api.particle.io/v1/devices/1e003d000d47343233323032/relayOff -d arg="4")" >> /var/log/lights.log
--- resources/lights-on.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-access_token=abc123
-
-front_lights_on=$(curl -s --connect-timeout 10 -m 10 -XPOST -H "Authorization: Bearer $access_token" https://api.particle.io/v1/devices/1e003d000d47343233323032/relayOn -d arg="3") 
-echo "$(date -Iseconds) front on $front_lights_on" >> /var/log/lights.log
-
-wreath_lights_on=$(curl -s --connect-timeout 10 -m 10 -XPOST -H "Authorization: Bearer $access_token" https://api.particle.io/v1/devices/1e003d000d47343233323032/relayOn -d arg="4")
-echo "$(date -Iseconds) wreath on $wreath_lights_on" >> /var/log/lights.log
